5d624249b287fe63b1b2e08d6ba33d93c99555ea
Arka2001 <sayantan.dasgupta21@gmail.com>
2022-08-17 02:24:51 +0530

    Added endpoint and logic for /api/date

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+function checkValidDate(date) {
+  var timestamp = Date.parse(date);
+
+  return isNaN(timestamp);
+}
+
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,7 +28,35 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
+});
+
+app.get("/api/:date", function (req, res) {
+  const timestamp = req.params.date;
+  console.log(timestamp);
+
+  // if(checkValidDate(timestamp) == false) {
+  //   res.json({error: "Invalid Date"});
+  // }
+  var dateOutput = "";
+  var unixOutput = "";
+
+  if (!timestamp.includes("-")) {
+    unixOutput = parseInt(timestamp);
+    let date = new Date(unixOutput);
+    res.json({
+      unix: unixOutput,
+      utc: date.toUTCString(),
+    });
+  } else {
+    let date = new Date(timestamp);
+
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    })
+  }
+
 });
 
 
